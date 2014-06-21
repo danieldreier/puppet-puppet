4b7e90266cfd7a7e4bba15fab22b932ea6a37988
Daniel Dreier <ddreier@thinkplango.com>
2014-06-21 13:13:18 -0700

    Update module list in spec_helper_acceptance.rb lock puppetlabs/ruby to 0.1.1 because master includes non-existent packages on debian 7 switch from puppetlabs-operations/puppet-nginx to jfryman/puppet-nginx standardize formatting

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -36,16 +36,17 @@ masters.each do |host|
 
   puppetfile = <<-EOS
 mod 'stdlib',         :git => 'git://github.com/puppetlabs/puppetlabs-stdlib.git'
-mod 'apt',         :git => 'git://github.com/puppetlabs/puppetlabs-apt.git'
+mod 'apt',            :git => 'git://github.com/puppetlabs/puppetlabs-apt.git'
 mod 'concat',         :git => 'git://github.com/puppetlabs/puppetlabs-concat.git'
-mod 'ruby',           :git => 'git://github.com/puppetlabs/puppetlabs-ruby.git'
+mod 'ruby',
+  :git => 'git://github.com/puppetlabs/puppetlabs-ruby.git',
+  :ref => '0.1.1'
 mod 'puppetlabs_yum', :git => 'git://github.com/stahnma/puppet-module-puppetlabs_yum'
-mod 'puppetlabs_apt', :git => 'git://github.com/puppetlabs-operations/puppet-puppetlabs_apt.git'
 mod 'interval',       :git => 'git://github.com/puppetlabs-operations/puppet-interval.git'
 mod 'unicorn',        :git => 'git://github.com/puppetlabs-operations/puppet-unicorn.git'
 mod 'rack',           :git => 'git://github.com/puppetlabs-operations/puppet-rack.git'
 mod 'bundler',        :git => 'git://github.com/puppetlabs-operations/puppet-bundler.git'
-mod 'nginx',          :git => 'git://github.com/puppetlabs-operations/puppet-nginx.git'
+mod 'nginx',          :git => 'git://github.com/jfryman/puppet-nginx.git'
 mod 'inifile',        :git => 'git://github.com/puppetlabs/puppetlabs-inifile.git'
 mod 'apache',         :git => 'git://github.com/puppetlabs/puppetlabs-apache.git'
 mod 'portage',        :git => 'git://github.com/gentoo/puppet-portage.git'
