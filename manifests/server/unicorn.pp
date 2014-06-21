7a81ffaf2fc40ef05af20ae154f3594d659e2b50
Daniel Dreier <ddreier@thinkplango.com>
2014-06-20 18:50:58 -0700

    Switch from ploperations/nginx to jfryman/nginx Still using ploperations/unicorn Still has undocumented dependency on ploperations/rack Depends on github master version of jfryman/nginx until next release

--- manifests/server/unicorn.pp
+++ manifests/server/unicorn.pp
@@ -2,34 +2,43 @@ class puppet::server::unicorn {
 
   include puppet::params
   include puppet::server::rack
-  include nginx::server
+  class { 'nginx':
+    proxy_connect_timeout => '300', # not in released jfryman/puppet-nginx yet
+  }
 
   class { 'puppet::server::standalone':
     enabled => false,
     before  => [
-      Nginx::Unicorn['puppetmaster'],
+      Nginx::Resource::Vhost['puppetmaster'],
       Unicorn::App['puppetmaster'],
     ],
   }
 
   $servername     = pick($::puppet::server::servername, $::clientcert, $::fqdn)
-  $unicorn_socket = "${puppet::params::puppet_rundir}/puppetmaster_unicorn.sock"
+  $unicorn_socket = "unix:${puppet::params::puppet_rundir}/puppetmaster_unicorn.sock"
 
-  nginx::unicorn { 'puppetmaster':
-    servername        => $servername,
-    path              => $::puppet::params::puppet_confdir,
-    unicorn_socket    => $unicorn_socket,
-    ssl               => true,
-    sslonly           => true,
-    ssl_port          => '8140',
-    ssl_cert          => "${::puppet::ssldir}/certs/${servername}.pem",
-    ssl_key           => "${::puppet::ssldir}/private_keys/${servername}.pem",
-    ssl_ca            => "${::puppet::ssldir}/certs/ca.pem",
-    ssl_crl_path      => "${::puppet::ssldir}/crl.pem",
-    ssl_ciphers       => $::puppet::server::ssl_ciphers,
-    ssl_protocols     => $::puppet::server::ssl_protocols,
-    ssl_verify_client => 'optional',
-    magic             => "proxy_connect_timeout 300s;\n  proxy_read_timeout 300s;",
+  nginx::resource::vhost { 'puppetmaster':
+    server_name           => [$servername],
+    www_root              => $::puppet::params::puppet_confdir,
+    ssl                   => true,
+    ssl_port              => '8140',
+    listen_port           => '8140', # force ssl_only by matching ssl_port
+    ssl_cert              => "${::puppet::ssldir}/certs/${servername}.pem",
+    ssl_key               => "${::puppet::ssldir}/private_keys/${servername}.pem",
+    ssl_ciphers           => $::puppet::server::ssl_ciphers,
+    ssl_protocols         => $::puppet::server::ssl_protocols,
+    proxy_read_timeout    => '300',
+    vhost_cfg_append      => {
+      ssl_crl                => "${::puppet::ssldir}/crl.pem",
+      ssl_client_certificate => "${::puppet::ssldir}/certs/ca.pem",
+      ssl_verify_client      => 'optional',
+    }
+  }
+
+  nginx::resource::upstream { $servername:
+    members => [
+      $unicorn_socket
+    ],
   }
 
   unicorn::app { 'puppetmaster':
